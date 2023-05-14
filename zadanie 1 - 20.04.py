8bb3d56136977d4d360b54fd7959aa8202e0109c
Angelika <angelikasor97@gmail.com>
2023-05-15 00:07:40 +0200

    Python_zadania

---
+++ zadanie 1 - 20.04.py
@@ -0,0 +1,6 @@
+name = input ("Podaj swoje imię ")
+age = int(input ("Podaj ile masz lat "))
+if age < 32:
+    print ("Mów mi Python, mam 32 lat. Witaj w moim świecie " + name + ". Jesteś młodszy ode mnie.")
+if age > 32:
+    print ("Mów mi Python, mam 32 lat. Witaj w moim świecie " + name + ". Jesteś starszy ode mnie.")
---
+++ zadanie 3 - 20.04.py
@@ -0,0 +1,4 @@
+zdanie = input("Podaj zdanie: ")
+slowa = zdanie.split()
+ilosc_slow = len(slowa)
+print(ilosc_slow)
